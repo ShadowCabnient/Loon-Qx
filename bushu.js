f6d4de4eb564430dc0c92c0c12333ca4c57336a3
ShadowCabnient <72256577+ShadowCabnient@users.noreply.github.com>
2022-10-18 21:37:19 +0800

    Create bushu.js

---
+++ bushu.js
@@ -0,0 +1,130 @@
+let obj = JSON.parse($response.body);
+obj ={
+  "status": "ok",
+  "data": {
+    "premium_data": {
+      "product": null,
+      "created": null,
+      "started": null,
+      "next_charge": null,
+      "expiration": null,
+      "cancelled": null,
+      "platform": null,
+      "productId": null,
+      "market": null,
+      "subscriptionType": null,
+      "type": null,
+      "subscriptionId": null,
+      "providerId": null,
+      "current_plan": null,
+      "upgrade_plan": null,
+      "suspended_plan": null,
+      "partner": null,
+      "partner_metadata": null,
+      "in_app_cancellation": false,
+      "subscription_status": "no_active_subscription",
+      "free_trial": {
+        "free_trial": true
+      }
+    },
+    "uid": 110711333,
+    "has_avatar": false,
+    "avatar": "https:\/\/cdn.busuu.com\/files\/default-avatars\/personas-avatars-08_500x500_cr.jpg",
+    "avatar_variations": {
+      "small": "https:\/\/cdn.busuu.com\/files\/imagecache\/user_image_default\/files\/default-avatars\/personas-avatars-08_500x500_cr.jpg",
+      "large": "https:\/\/cdn.busuu.com\/files\/imagecache\/180x180\/files\/default-avatars\/personas-avatars-08_500x500_cr.jpg",
+      "500_500": "https:\/\/cdn.busuu.com\/files\/imagecache\/500x500\/files\/default-avatars\/personas-avatars-08_500x500_cr.jpg",
+      "original": "https:\/\/cdn.busuu.com\/files\/imagecache\/500x500\/files\/default-avatars\/personas-avatars-08_500x500_cr.jpg"
+    },
+    "friends": 0,
+    "thumbs": {
+      "made": 0,
+      "received": 0,
+      "total": 0,
+      "percentage": 0,
+      "likes_received": 0
+    },
+    "exercises_done": 0,
+    "corrections_done": 0,
+    "best_corrections_awarded": 0,
+    "name": "周康捷",
+    "city": null,
+    "about_me": null,
+    "languages": {
+      "interface": "zh",
+      "learning_default": "en",
+      "learning": {
+        "en": "beginner"
+      },
+      "spoken": {
+        "zh": "native"
+      }
+    },
+    "country": "中国香港特别行政区",
+    "country_code": "HK",
+    "flag": "https:\/\/cdn.busuu.com\/assets\/8f5f0ae2\/images\/flags\/HK.png",
+    "shadow_ban": false,
+    "spoken_language_chosen": false,
+    "is_premium": true,
+    "currency_code": "EUR",
+    "timezone": "Asia\/Shanghai",
+    "is_tutor": false,
+    "last_login": 1666090203,
+    "created": 1666090201,
+    "last_access": 1666090215,
+    "email": "445165653@qq.com",
+    "phone_data": {},
+    "unique_username": null,
+    "access_token": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjVkMTlhNWFhLThmNzYtNDUzMC04ODgxLWYzYzA3MzM5ODdlYSJ9.eyJleHAiOjE2ODE4NzAyMDMsImRhdGEiOnsidXNlcl9sZWdhY3lfaWQiOjExMDcxMTMzM30sImlhdCI6MTY2NjA5MDIwMywiYXVkIjoiYnVzdXUiLCJpc3MiOiJjb20uYnVzdXUudG9rZW4iLCJzdWIiOiI2OGI2NmI0Ni1kYTllLTQ5OWEtYmUyMi02NzY3ZGUwZTM3NTkiLCJqdGkiOiJiOWM2MTExMi0zMWIzLTRlNmUtYjA5Mi1jYWFlZTVhZmQ2NTQifQ.CE7oi2Sp-sCh7C-bJQbHCKZN6puJZ4WPWfDR-lbID95RKKTSif2G2iHSmwmMmvqVrz5H2I2m2ZPROdenySmEHLF-1tcATtEXtiVDA6EOgJeehKpvq0_xCU_qOOMnyc1bZ4lhP6iiWMf6Y_VGlxZQQGjsy3fyJXpCogB77SKyQkdLqRVecQHQTP_y58Q2MtydxMZ9a-Ro7YHuSvRvNpv1jryQFJHXTw11J-_vdH5csy98iZWao1FBSq2iPpkIAmqb5i-3bjbR9doc_QinqVmBqsNAkJffvhx5q6rbHp6UvpkIhH-IQjR0wdkGk83u39um82mChRgvDMvVmw2P9YC-WMtyt2XXYY0RiTt8YRzlABPdpvRNocOaEzM0YNOVtU9BNEKVU0GwW-P47mP8amRM77zajd4u8MWVv-raO0VDCwuHEIUA-xBxVZgKt6KBlWfwoWYfY1Wr9G-dg8PKGnfSD6DvHcMMSd04GRx22JFc0lpJvKTjbWYhTxcWt-_b2cshG3t5A5cEttAMFSljnhi1sL1gmdteTE98CVJXv5brTgjWNwDlQs3WCKAtTjuC-ozqaS3TZwiGHIU4VKJcwXB4iUeG08m7PUwAeFqhROiTZ-HedcxdFlHFu9lfJrxYDXGhVPHO5leVFmNgx3L1AjpFRXIWxRzORqYX4V-KHew8xPo",
+    "is_impersonated": false,
+    "fb_uid": null,
+    "g_uid": null,
+    "apple_user_id": "001096.eea3462ce6ad4e1b8d323bfeb888b260.1049",
+    "opt_in_promotions": true,
+    "profile_name": "周康捷",
+    "first_name": "周康捷",
+    "birthday": null,
+    "checklist_complete": true,
+    "occupation": null,
+    "notification_correction_received": true,
+    "notification_correction_added": true,
+    "notification_correction_requested": true,
+    "notification_correction_replies": true,
+    "notification_friend_requested": true,
+    "notification_friend_replied": true,
+    "notification_mute_all": false,
+    "private_mode_friend_requested": false,
+    "notification_study_plan": false,
+    "notification_leaderboard": true,
+    "premium_provider": null,
+    "course_access": {
+      "travel": false,
+      "business": [],
+      "mobile": []
+    },
+    "roles": [],
+    "placement_chooser": true,
+    "placement_test_available": {
+      "de": true,
+      "es": true,
+      "fr": true,
+      "it": true,
+      "pt": true,
+      "nl": true,
+      "en": true
+    },
+    "free_trial_eligible": false,
+    "access": {
+      "tier": "plus"
+    },
+    "referral_token": "bWxJM0pPZGsyYm9j",
+    "referral_url": "https:\/\/get.busuu.com\/bWxJM0pPZGsyYm9j",
+    "advocate_id": null,
+    "verbling_user_id": null,
+    "uuid": "68b66b46-da9e-499a-be22-6767de0e3759",
+    "hashed_email": "f5d52b85e5bdfef415497d8a3abc30152cb26d8e775bdb0a3c409677bb9276ba",
+    "current_course_pack": "course_pack_en_complete",
+    "is_friend": null
+  }
+};
+$done({body: JSON.stringify(obj)});
