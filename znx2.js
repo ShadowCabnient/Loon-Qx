85e05635a4b3369e1c968e92df7be61e103f93fd
ShadowCabnient <72256577+ShadowCabnient@users.noreply.github.com>
2022-10-22 17:06:49 +0800

    Update znx2.js

--- znx2.js
+++ znx2.js
@@ -3,282 +3,282 @@ obj ={
   "limit-compute": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "series-limit": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "func-deri": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "mean-value": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "der-application": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "der-proof": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "int-compute": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "def-int-comp": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "int-application": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "2d-der": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "2der-compute": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "2d-int": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "diff-equation": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "surface-int": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "curve-int": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "series-sum": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "power-series": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "limit-comp-book1": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "series-limit-book1": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "limit-comp-book-1-9": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "series-limit-book1-6": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "discont": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "deri-2-1": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "deri-2-2": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "mean-value-3-1": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "limit-comp-3-2": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "int-basic": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "int-4-2": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "int-4-3": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "int-4-4": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "int-5-2": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "int-5-4": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "int-5-5": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "diff-eqn-7-5": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "diff-eqn-7-7": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "diff-eqn-7-8": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "der-app-3-4": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "der-app-3-5": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "int-app-6-3": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "2d-der-9-1": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "2d-der-9-3": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "2d-der-9-5": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "2d-der-9-8": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "2d-int-10-2-2": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "2d-int-10-2-4": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "2d-int-10-3": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "curve-int-11-2": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "curve-int-11-3": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "surface-int-11-5": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "surface-int-11-7": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "series-sum-12-1": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "series-sum-12-2-1": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "series-sum-12-2-2": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "power-series-12-3": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "power-series-12-4": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   },
   "fourier-12-8": {
     "pairingStatus": "INITIAL",
     "paringCode": "",
-    "expireTime": "2022-12-31T17:32:13.503Z"
+    "expireTime": "2022-12-31T17:32:13.503Z",
   }
 };
 $done({body: JSON.stringify(obj)});
