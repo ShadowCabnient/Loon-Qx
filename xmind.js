73537b70aee72fcd5873228dc0bf99c2ea981369
ShadowCabnient <72256577+ShadowCabnient@users.noreply.github.com>
2022-11-01 16:05:13 +0800

    Update xmind.js

--- xmind.js
+++ xmind.js
@@ -1,1 +1,1 @@
-var _0x49754b=JSON['parse']($response['body']);_0x49754b['license']['status']='sub';_0x49754b['license']['expireTime']=0x1b8d90e4000;$done({'body':JSON['stringify'](_0x49754b)});
+var _0x49754b=JSON['parse']($response['body']);_0x49754b['license']['status']='sub';_0x49754b['license']['expireTime']= 0x29C9B8F1800;$done({'body':JSON['stringify'](_0x49754b)});
