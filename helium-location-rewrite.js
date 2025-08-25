43489c7903870322e87429204377e2169ed270b1
ShadowCabnient <72256577+ShadowCabnient@users.noreply.github.com>
2025-08-25 09:32:24 +0800

    Update helium-location-rewrite.js

--- helium-location-rewrite.js
+++ helium-location-rewrite.js
@@ -3,7 +3,7 @@
  * 用途：修改 Helium location 请求中的 is_moving 字段
  * 
   [rewrite_local]
-  ^https://sublocsvc\.hmprod\.nova\.xyz:5090/locations url script-request-body helium-location-rewrite.js
+  ^https://sublocsvc\.hmprod\.nova\.xyz:5090/locations url script-request-body https://raw.githubusercontent.com/ShadowCabnient/Loon-Qx/refs/heads/main/helium-location-rewrite.js
   
   [mitm]
 hostname = sublocsvc.hmprod.nova.xyz
